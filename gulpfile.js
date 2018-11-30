82da9c1e5ac7f30af298ca332a6b825373d746ee
jjonathan <jmachado@uplexis.com.br>
2018-11-30 10:57:06 -0200

    Removido muita coisa

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-const gulp = require("gulp")
-const sass = require("gulp-sass")
-const watchSass = require("gulp-watch-sass")
- 
-gulp.task("sass:watch", () => watchSass([
-  "./public/scss/import.scss"
-])
-.pipe(sass())
-.pipe(gulp.dest("./public")));
